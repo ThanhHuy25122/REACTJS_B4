b0818ae7884a93fb75eec23b977d81e839d8d608
ThanhHuy25122 <huyxh251@gmail.com>
2022-12-20 23:13:14 +0700

    start

---
+++ buoi-4/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BaiTapDatVeXemPhim from "./components/BaiTapDatVeXemPhim/BaiTapDatVeXemPhim";
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapDatVeXemPhim />
+    </div>
+  );
+}
+
+export default App;
